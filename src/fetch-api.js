f0e689d4100dbf8aeb6f134d4292a66f49bec8e9
Julie Shapovalenko <jools.shapo@gmail.com>
2023-03-13 11:02:08 +0200

    add fetch.js and update index.js

---
+++ src/fetch-api.js
@@ -0,0 +1,9 @@
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,language`)
+        .then(response => {
+         if (response.status === 404) {
+                throw new Error(response.status);
+            }
+        return response.json();
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,75 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import { fetchCountries } from './fetch-api'
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    list: document.querySelector('.country-list'),
+    info: document.querySelector('.country-info'),
+}
+
+refs.input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY))
+
+function onInputChange(e) {
+    const searchCountry = e.target.value.trim();
+    cleanHtml();
+
+    if (searchCountry !== 0) {
+        fetchCountries(searchCountry).then(data => {
+            if (data.length > 10) {
+                alert("Too many matches found. Please enter a more specific name.");
+            }
+
+            else if (data.length >= 2 || data.length <= 10) {
+                createCountriesListMarkup(data);
+            }
+
+            else if (data === 0) {
+                alert("Oops, there is no country with that name")
+            }
+
+            else if (data.length === 1) {
+                createCountryInfoMarkup(data);
+            }
+            console.log(data)
+        })
+        .catch((error) => {console.log('error', error)})  
+    }
+}
+
+function createCountriesListMarkup(countries) {
+    const markupCountriesList = countries.map(country =>
+    { return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${country.name.official
+        }" width="30" hight="20">
+         <p>${country.name.official}</p>
+                </li>` }).join('');
+
+    refs.list.insertAdjacentHTML('beforeend', markupCountriesList);
+}
+
+function createCountryInfoMarkup(countries) {
+    const markupCountryInfo = countries.map(country => {
+        return `<div>
+        <img
+          src="${country.flags.svg}"
+          alt="Flag of ${country.name.official}"
+          width="30"
+          hight="20"
+        />
+        <p>${country.name.official}</p>
+        <p>Capital: ${country.capital}</p>
+        <p>Population: ${country.population}</p>
+        <p>Languages: ${Object.values(country.languages)}</p>
+      </div>`
+    }).join('');
+    
+    refs.info.innerHTML = markupCountryInfo;
+}
+
+function cleanHtml() {
+    refs.list.innerHTML = '';
+    refs.info.innerHTML = '';
+}
